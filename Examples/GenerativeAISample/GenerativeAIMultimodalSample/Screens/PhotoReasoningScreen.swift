37532f4b4679ed9bc5a9173af10da1defc5889c9
DEmpire412 <105176197+DEmpire412@users.noreply.github.com>
2024-06-23 00:26:37 -0400

    Update PhotoReasoningScreen.swift

--- Examples/GenerativeAISample/GenerativeAIMultimodalSample/Screens/PhotoReasoningScreen.swift
+++ Examples/GenerativeAISample/GenerativeAIMultimodalSample/Screens/PhotoReasoningScreen.swift
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import GenerativeAIUIComponents
+import GeminiAIUIComponents
 import MarkdownUI
 import PhotosUI
 import SwiftUI
